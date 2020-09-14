145e0312bd1572efa648d517dd790ffac76656bc
manzari <22736528+manzari@users.noreply.github.com>
2020-09-15 00:43:03 +0200

    fix NotFoundResponse and NotFoundOrNoRightsResponse texts

--- src/Responses/NotFoundOrNoRightsResponse.php
+++ src/Responses/NotFoundOrNoRightsResponse.php
@@ -7,10 +7,9 @@ class NotFoundOrNoRightsResponse extends ErrorResponse
 {
     public function __construct(string $objectName, bool $plural = false)
     {
-        $doDoes = $plural ? 'do' : 'does';
-        $themIt = $plural ? 'them' : 'it';
         $message = 'The ' . $objectName . ' you are looking for '
-            . $doDoes . ' not exist or you have no right to view ' . $themIt . '!';
-        parent::__construct($message, self::HTTP_BAD_REQUEST, true);
+        . ($plural ? 'do' : 'does') . ' not exist or you have no right to view '
+        . ($plural ? 'them' : 'it') . '!';
+        parent::__construct($message, self::HTTP_BAD_REQUEST);
     }
 }
--- src/Responses/NotFoundResponse.php
+++ src/Responses/NotFoundResponse.php
@@ -8,7 +8,7 @@ class NotFoundResponse extends ErrorResponse
 {
     public function __construct(?string $objectName = null)
     {
-        $message = 'The ' . $objectName ?? 'object' . ' you are looking for does not exist!';
+        $message = 'The ' . ($objectName ?? 'object') . ' you are looking for does not exist!';
         parent::__construct($message, self::HTTP_NOT_FOUND, true);
     }
 }
